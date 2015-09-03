15683934d5947b1ab80ba2576627fb4455afa01d
Hunter Perrin <hperrin@gmail.com>
2015-09-02 18:05:46 -0700

    Changing indentation style to four spaces.

--- testing/TestModel.php
+++ testing/TestModel.php
@@ -1,13 +1,13 @@
 <?php
 
 class TestModel {
-	public $test = 'right';
+    public $test = 'right';
 
-	public function testFunction($argument) {
-		return $argument;
-	}
+    public function testFunction($argument) {
+        return $argument;
+    }
 
-	public function testFunctionFake($argument) {
-		return false;
-	}
+    public function testFunctionFake($argument) {
+        return false;
+    }
 }
